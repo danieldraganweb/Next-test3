ef59f9d7c3ebe57dc7eb4523e0302a12e0a4e8b1
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-01-31 14:57:49 +0100

    Capital letter for products, just for my OCD haha

--- interviewtask2023-1/pages/warehouse.tsx
+++ interviewtask2023-1/pages/warehouse.tsx
@@ -5,27 +5,27 @@ const Warehouse = () => {
   const [inventory] = useState([
     {
       art_id: "1",
-      name: "leg",
+      name: "Leg",
       stock: 12,
     },
     {
       art_id: "2",
-      name: "screw",
+      name: "Screw",
       stock: 17,
     },
     {
       art_id: "3",
-      name: "seat",
+      name: "Seat",
       stock: 2,
     },
     {
       art_id: "4",
-      name: "table top",
+      name: "Table top",
       stock: 1,
     },
   ]);
 
-  const [products] = useState([
+  const [products, setProducts] = useState([
     {
       name: "Dining Chair",
       contain_articles: [
@@ -62,34 +62,63 @@ const Warehouse = () => {
     },
   ]);
 
+  const handleAddAmount = (
+    productIndex: number,
+    articleIndex: number,
+    amount: number
+  ) => {
+    const newProducts = [...products];
+    newProducts[productIndex].contain_articles[articleIndex].amount_of +=
+      amount;
+    setProducts(newProducts);
+  };
+
   return (
     <div>
-      <h1 className={styles.inventory}>Inventory</h1>
-      <h1 className={styles.available}>Available Products</h1>
+      <div className={styles.inventory}>
+        <h1>Inventory</h1>
+      </div>
+      <div className={styles.productlist}>
+        <h1 className={styles.available}>Available Products</h1>
 
-      <ul>
-        {inventory.map((item) => (
-          <li key={item.art_id}>
-            {item.name} - {item.stock}
-          </li>
-        ))}
-      </ul>
+        <ul>
+          {inventory.map((item) => (
+            <li key={item.art_id}>
+              {item.name} - {item.stock}
+            </li>
+          ))}
+        </ul>
 
-      <h1>Products</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.name}>
-            {product.name}
-            <ul>
-              {product.contain_articles.map((article) => (
-                <li key={article.art_id}>
-                  {article.art_id} - {article.amount_of}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+        <h1>Products</h1>
+        <ul>
+          {products.map((product, productIndex) => (
+            <li key={product.name}>
+              {product.name}
+              <ul>
+                {product.contain_articles.map((article, articleIndex) => (
+                  <li key={article.art_id}>
+                    {article.art_id} - {article.amount_of}
+                    <button
+                      onClick={() =>
+                        handleAddAmount(productIndex, articleIndex, -1)
+                      }
+                    >
+                      -
+                    </button>
+                    <button
+                      onClick={() =>
+                        handleAddAmount(productIndex, articleIndex, 1)
+                      }
+                    >
+                      +
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
