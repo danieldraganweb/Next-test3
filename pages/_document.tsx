2744dfab8c13b93d90425a2f1554b09b74e61f46
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-02-07 16:54:12 +0100

    Comments add to the components index and document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
+// This is a custom Next.js Document component
 export default function Document() {
   return (
+    // Defines the HTML document structure
     <Html lang="en">
+      {/* A head tag to contain metadata and links */}
       <Head />
+      {/* A body tag to contain the main content */}
       <body>
+        {/* The main content of the page */}
         <Main />
+        {/* The script for Next.js */}
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
